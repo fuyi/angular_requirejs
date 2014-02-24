eb1aa359dda9fab2e80efbc127629356c4db39f0
fuyi <yi@whaam.com>
2014-02-24 15:21:26 +0100

    make some change on view1

--- js/controllers/my-ctrl-1.js
+++ js/controllers/my-ctrl-1.js
@@ -1,4 +1,7 @@
 define(['./module'], function (controllers) {
     'use strict';
-    controllers.controller('MyCtrl1', [function ($scope) {}]);
+    controllers.controller('MyCtrl1', ['$scope',function ($scope) {
+        $scope.first_name = 'Yi';
+        $scope.last_name = 'FU';
+    }]);
 });
